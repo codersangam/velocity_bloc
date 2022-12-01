2ff2cb073af579e2e925a0bc60752473fef63060
codersangam <hritriik@gmail.com>
2022-12-01 21:15:54 +0400

    added show hide data example

---
+++ example/lib/screens/show_hide_data/show_hide_data.dart
@@ -0,0 +1,49 @@
+part of 'show_hide_data_imports.dart';
+
+class ShowHideData extends StatefulWidget {
+  const ShowHideData({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<ShowHideData> createState() => _ShowHideDataState();
+}
+
+class _ShowHideDataState extends State<ShowHideData> {
+  @override
+  Widget build(BuildContext context) {
+    ShowHideDataViewModel showHideDataViewModel = ShowHideDataViewModel();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: BlocBuilder<VelocityBloc<bool>, VelocityState<bool>>(
+        bloc: showHideDataViewModel.isHidden,
+        builder: (context, state) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              !state.data
+                  ? "123456".text.xl3.makeCentered()
+                  : "******".text.xl3.makeCentered(),
+              SizedBox(
+                height: 50,
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    showHideDataViewModel.isHidden
+                        .onUpdateData(state.data.toggle());
+                  },
+                  child: !state.data
+                      ? "Hide Data".text.make()
+                      : "Show Data".text.makeCentered(),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ example/lib/screens/show_hide_data/show_hide_data_view_model.dart
@@ -0,0 +1,5 @@
+part of 'show_hide_data_imports.dart';
+
+class ShowHideDataViewModel {
+  VelocityBloc<bool> isHidden = VelocityBloc<bool>(false);
+}
